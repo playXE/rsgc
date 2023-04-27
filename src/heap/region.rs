2e6d4d558f3769d448fd7996574183248ff1bc34
playXE <adel.prokurov@gmail.com>
2023-04-27 19:09:01 +0400

    default 2 threads

--- src/heap/region.rs
+++ src/heap/region.rs
@@ -497,11 +497,7 @@ impl Default for HeapArguments {
             tlab_waste_increment: 4,
             tlab_waste_target_percent: 1,
             parallel_region_stride: 1024,
-            parallel_gc_threads: if num_cpus::get_physical() / 4 == 0 {
-                1
-            } else {
-                num_cpus::get_physical() / 4
-            },
+            parallel_gc_threads: 2,
 
             max_satb_buffer_flushes: 5,
             max_satb_buffer_size: 1024,
